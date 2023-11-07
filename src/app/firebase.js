0b535ab7cc203a55597f6c95b2b24b48e02ce11c
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2023-11-07 15:02:35 -0300

    SignUpStyle

---
+++ src/app/firebase.js
@@ -0,0 +1,17 @@
+    // Import the functions you need from the SDKs you need
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+    apiKey: "AIzaSyDFQu8YSl8QHqJitcPrFaguDVRvlZdtmTg",
+    authDomain: "fir-userbackend.firebaseapp.com",
+    projectId: "fir-userbackend",
+    storageBucket: "fir-userbackend.appspot.com",
+    messagingSenderId: "881590666215",
+    appId: "1:881590666215:web:c3da7eb8631f65a3f57342"
+    };
+
+    // Initialize Firebase
+    export const app = initializeApp(firebaseConfig);
---
+++ src/main.js
@@ -0,0 +1,3 @@
+import './app/firebase.js'
+
+console.log("Hello World")
