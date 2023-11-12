83fe5ea65db0f3b09621567413dbef6263820478
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2023-11-12 07:22:50 -0300

    error.alert

--- src/app/signupForm.js
+++ src/app/signupForm.js
@@ -1,23 +1,31 @@
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
-
 import { auth } from './firebase.js';
 
-const signupForm = document.querySelector("#SignUpForm")
+const signupForm = document.querySelector("#SignUpForm");
 
 signupForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
 
-    e.preventDefault()
+    const email = signupForm['SingUpEmail'].value;
+    const password = signupForm['SingUpPassword'].value;
 
-    const email = signupForm['SingUpEmail'].value
-    const password = signupForm['SingUpPassword'].value
+    console.log(email, password);
 
-    console.log(email, password)
+    try {
+        const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+        window.location.href = "./userValidated.html";
 
-    try{
-        const userCredentials = await createUserWithEmailAndPassword(auth, email, password)
-        console.log(userCredentials)
     } catch (error) {
-        console.log(error)
-    }
 
-})
+        if (error.code === 'auth/invalid-email') {
+            alert('Invalid email format. Please enter a valid email address.');
+        } else if (error.code === 'auth/weak-password') {
+            alert('Password is too weak. Please choose a stronger password (6 > characters).');
+        } else if (error.code === 'auth/email-already-in-use') {
+            alert('The email address is already in use by another account.');
+        } else {
+            alert('An error occurred during registration. Please try again.');
+        }
+    }
+});
+
